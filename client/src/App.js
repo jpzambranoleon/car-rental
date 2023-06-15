f47e49f49d8de09df3707136d9d05602ab3a1347
Jean-Paul <jpzambrano.leon@gmail.com>
2023-06-15 17:27:43 -0500

    modified file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route index path="/" element={<Home />} />
         </Routes>
