6e9e5c9292a6c8562606b94b648910cee58ed961
Jean-Paul <jpzambrano.leon@gmail.com>
2023-06-15 18:53:19 -0500

    modified file

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import BookCar from "../components/BookCar";
 import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <BookCar />
     </>
   );
 }
