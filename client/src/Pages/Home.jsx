98c5091fad68fa1a6e74bbc1df68f06eda9ddd4d
Jean-Paul <jpzambrano.leon@gmail.com>
2023-06-15 20:49:33 -0500

    modified Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,27 @@
+import Banner from "../components/Banner";
 import BookCar from "../components/BookCar";
+import ChooseUs from "../components/ChooseUs";
+import Download from "../components/Download";
+import Faq from "../components/Faq";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
+import PickCar from "../components/PickCar";
+import PlanTrip from "../components/PlanTrip";
+import Testimonials from "../components/Testimonials";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <BookCar />
+      <PlanTrip />
+      <PickCar />
+      <Banner />
+      <ChooseUs />
+      <Testimonials />
+      <Faq />
+      <Download />
+      <Footer />
     </>
   );
 }
